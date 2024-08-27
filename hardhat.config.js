9ce1e1a9f02dadcf1204da2babe3d95ab7f6e649
Jack Lee <280147597@qq.com>
2024-08-27 23:52:04 +0800

    add scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    confluxTestnet: {
+      url: "https://evmtestnet.confluxrpc.com",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 71
+    }
+  }
 };
